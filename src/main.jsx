47700554b5ad5d4eb920b86f235f48db54302b98
Dries Mangelschots <r0590700@student.thomasmore.be>
2022-10-10 19:18:07 +0200

    Navbar + error page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Routing from './components/Routing';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <BrowserRouter>
+    <Navbar />
+    <Routing />
+
+  </BrowserRouter>
   </React.StrictMode>
 )
